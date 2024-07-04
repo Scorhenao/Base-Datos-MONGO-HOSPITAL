5c8f4bc32b368627edd1c328fe9f20c5410199a6
Scorhenao <161768722+Scorhenao@users.noreply.github.com>
2024-07-04 16:19:23 -0400

    consult for admin 1 with mongoDB

--- app/consults/administradores/1Admin.sql
+++ app/consults/administradores/1Admin.sql
@@ -1,6 +1,35 @@
 - Conocer el número de pacientes que se encuentran en UCI y en cada sala.
-SELECT SALAS_UCI.ID_salaUCI, COUNT(CAMAS.ID_paciente_FK) AS cantidad_pacientes
-FROM CAMAS
-JOIN SALAS_UCI ON CAMAS.ID_salaUCI_FK = SALAS_UCI.ID_salaUCI
-WHERE CAMAS.ID_paciente_FK IS NOT NULL AND CAMAS.Estado = 'ocupado'
-GROUP BY SALAS_UCI.ID_salaUCI;
+db.CAMAS.aggregate([
+    {
+        $match: {
+            "Paciente._id": { $exists: true },
+            "Estado": "Ocupada"
+        }
+    },
+    {
+        $group: {
+            _id: "$SalaUCI._id",
+            cantidad_pacientes: { $sum: 1 }
+        }
+    },
+    {
+        $lookup: {
+            from: "SALAS_UCI",
+            localField: "_id",
+            foreignField: "_id",
+            as: "sala_info"
+        }
+    },
+    {
+        $unwind: "$sala_info"
+    },
+    {
+        $project: {
+            _id: 0,
+            "sala_id": "$_id",
+            "descripcion_sala": "$sala_info.Descripcion",
+            "cantidad_pacientes": 1
+        }
+    }
+])
+
